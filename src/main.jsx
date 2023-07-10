accb67fe13059aed1eb018a0852bfa7a6567a930
Fabio Rizzi <f.rizzi@citron.io>
2023-07-10 14:25:57 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./routes/Home";
+import Program from "./routes/Program";
+import Course from "./routes/Course";
+import Login from "./routes/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/program",
+    element: <Program />,
+  },
+  {
+    path: "/course",
+    element: <Course />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
